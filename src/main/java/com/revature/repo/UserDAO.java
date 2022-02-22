03aa5d2b3940de4e2f3ebd1fe50514fc50c35cb2
Eric Florence <ericflorence.dev@gmail.com>
2022-02-22 15:38:54 -0700

    restart omg

--- src/main/java/com/revature/repo/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repo;
-
-import com.revature.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDAO extends JpaRepository<User, Integer> {
-    User findByUsername(String userName);
-
-}

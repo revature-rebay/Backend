1b2d585a15b0f66b69a819e055e736418558b047
Jessie Guadarrama <jesguadarrama98@gmail.com>
2022-02-22 14:47:55 -0800

    reinitialized

---
+++ src/main/java/com/revature/Project3backendApplication.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3backendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3backendApplication.class, args);
+	}
+
+}

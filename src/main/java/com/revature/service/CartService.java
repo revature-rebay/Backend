6d8acd60dc6b26fcd43e3adad9cc2035c1e5fca5
JBOhge <ohgejb@gmail.com>
2022-02-23 13:12:46 -0600

    productDAO + cartservice

--- src/main/java/com/revature/service/CartService.java
+++ src/main/java/com/revature/service/CartService.java
@@ -2,16 +2,22 @@ package com.revature.service;
 
 import com.revature.models.CartDTO;
 import com.revature.models.CartItem;
+import com.revature.models.Product;
+import com.revature.models.User;
+import com.revature.repo.ProductDAO;
 import com.revature.repo.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CartService {
 
     private UserDAO userDAO;
+    private ProductDAO productDAO;
 
     public CartService() {
     }
@@ -22,7 +28,7 @@ public class CartService {
     }
 
     public ArrayList<CartItem> getCart(int userId){
-        return null;
+        return new ArrayList<>();
     }
 
     public ArrayList<CartItem> addToCart(CartDTO item){
@@ -32,15 +38,36 @@ public class CartService {
 
     public ArrayList<CartItem> updateProductQuantity(CartDTO item){
 
-        return null;
+        return new ArrayList<>();
     }
 
     public ArrayList<CartItem> deleteFromCart(CartDTO item){
+        Optional<User> userOptional = userDAO.findById(item.userId);
+        if(!userOptional.isPresent()){
+            return new ArrayList<>();
+        }
+
+//        Optional<Product> productOptional =
+
+        User user = userOptional.get();
+        List<CartItem> cartItems = user.getCart();
+        user.setCart(cartItems);
+
+        userDAO.save(user);
+
         return null;
     }
 
     public boolean clearCart(int userId){
-        return false;
+        Optional<User> userOptional = userDAO.findById(userId);
+        if(!userOptional.isPresent()){
+            return false;
+        }
+
+        User user = userOptional.get();
+        user.setCart(new ArrayList<>());
+        userDAO.save(user);
+        return true;
     }
 
 

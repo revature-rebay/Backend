2d3fe60e234461cb652d63e588a1ee3f90b936b4
Elias Calagiu <eliasc81@gmail.com>
2022-02-23 09:02:58 -0800

    wrote tests for UserService

--- src/test/java/com/revature/service/UserServiceTest.java
+++ src/test/java/com/revature/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.revature.service;
 import com.revature.models.User;
 import com.revature.repo.UserDAO;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,31 +15,52 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(MockitoExtension.class)
-@SpringBootTest
+//@ExtendWith(MockitoExtension.class)
+//@SpringBootTest
 class UserServiceTest {
     @Mock
-    private UserDAO userDAO = Mockito.mock(UserDAO.class);
-    @InjectMocks
+    private UserDAO userDAO;
+//            = Mockito.mock(UserDAO.class);
+//    @InjectMocks
     private UserService userService;
+
+    private User testUser;
 
     @BeforeEach
     void initServiceTest(){
         userService= new UserService(userDAO);
+        testUser = new User(
+                1,
+                "eliasc81",
+                "password",
+                "me@me.com",
+                "Elias",
+                "Calagiu",
+                1
+        );
     }
 
     @Test
-    void getUser() {
-    }
-
-    @Test
+    @Order(1)
     void saveUser() {
-        User user = new User(1,"eliasc81", "password", "me@me.com", "Elias", "Calagiu", 1);
-        boolean savedUser= userService.saveUser(user);
+        boolean savedUser= userService.saveUser(testUser);
         assertTrue(savedUser);
     }
 
+
     @Test
+    @Order(2)
+    void getUser() {
+        User user = userService.getUser(testUser.getId());
+        assertEquals(user, testUser);
+    }
+
+
+
+    @Test
+    @Order(3)
     void validateAccount() {
+        User user = userService.validateAccount(testUser.getUserName(), testUser.getPassWord());
+        assertEquals(user, testUser);
     }
 }

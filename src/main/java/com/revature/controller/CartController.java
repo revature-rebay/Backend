27310ee777dd37c91140e370c71838c1f432764b
merry-degaga <merry.degaga@revature.net>
2022-02-23 14:26:26 -0500

    added cartController

---
+++ src/main/java/com/revature/controller/CartController.java
@@ -0,0 +1,56 @@
+package com.revature.controller;
+
+import com.revature.models.CartDTO;
+import com.revature.models.CartItem;
+import com.revature.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.xml.ws.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/cart")
+@CrossOrigin(value ="http://localhost:4200")
+public class CartController {
+    private CartService service;
+
+    public CartController() {
+    }
+
+    @Autowired
+    public CartController(CartService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<CartItem>> getCart(@PathVariable("userId") int userId){
+        List<CartItem> cart = service.getCart(userId);
+        if(cart.size() == 0) return ResponseEntity.status(204).body(cart);
+        return ResponseEntity.status(200).body(cart);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<List<CartItem>> addToCart(@RequestBody CartDTO cartDTO) {
+        return ResponseEntity.status(201).build();
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<List<CartItem>> updateProductQuantity(@RequestBody CartDTO cartDTO) {
+        return ResponseEntity.status(200).build();
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity deleteFromCart(@RequestBody CartDTO cartDTO) {
+        return ResponseEntity.status(200).build();
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity clearCart(@PathVariable("userId") int userId) {
+        return ResponseEntity.status(200).build();
+    }
+
+
+}

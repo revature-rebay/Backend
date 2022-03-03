c1be0bd044188d302dbdc91bb3a7a5f05f943ae2
Eric Florence <ericflorence.dev@gmail.com>
2022-03-03 10:40:50 -0700

    changed origin to httpS

--- src/main/java/com/revature/controller/UserController.java
+++ src/main/java/com/revature/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:4200", "http://d1fpc6erw3y64i.cloudfront.net"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:4200", "https://d1fpc6erw3y64i.cloudfront.net"}, allowCredentials = "true")
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
